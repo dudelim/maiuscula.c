c91c963cdb0ede0190bd638bfa0b9f87dc322699
dudelim <137728498+dudelim@users.noreply.github.com>
2023-07-06 19:25:49 +0000

    1

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
+int sget(char* vetor, int tam) {
+    fflush(stdin); 
+    if (fgets(vetor, tam, stdin)) { 
+        int i;
+        for( i = 0; vetor[i] != '\n' && vetor[i]; ++i)
+          ;
+        vetor[i] = '\0';
+    }
+}
+
+#define TAM 50
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  char frase[50];
+
+  printf("Input: ");
+  sget(frase, TAM);
 }
